5a9024120bee28c7b767bf7c3afa142ab2806614
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2025-05-29 22:33:17 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
+import { useState } from "react";
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import LoginForm from "./loginForm";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const handleLogin = () => {
+    setIsLogin(true);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div>
+        <h1>Lift State Up Demo</h1>
+        {isLogin ? (
+          <h2>Welcome, you are logged in!</h2>
+        ) : (
+          <LoginForm onLogin={handleLogin} />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
